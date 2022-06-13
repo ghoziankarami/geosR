9062e8204eb37338df81b209e0d9aca4f4288e58
ghoziankarami <ghoziankarami@gmail.com>
2022-06-13 15:11:02 +0700

    Initial Commit

---
+++ R/no_outlier.R
@@ -0,0 +1,10 @@
+#' Removing Outliers Function
+#'
+#' this function using the IQR method for calculating outliers removal
+
+no_outlier <- function(df, col){
+  Q1 <- data.table::quantile(df$col, .25)
+  Q3 <- data.table::quantile(df$col, .75)
+  IQR <- data.table::IQR(df$col)
+  no_outliers_data <- subset(df, df$col > (Q1 - 1.5*IQR) & df$col < (Q3 + 1.5*IQR))
+}
