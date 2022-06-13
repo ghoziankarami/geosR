2b37dea5fed4da199e0615cffd3a0d1f8a4ea9e0
ghoziankarami <ghoziankarami@gmail.com>
2022-06-13 18:49:43 +0700

    commit

--- R/no_outlier.R
+++ R/no_outlier.R
@@ -9,5 +9,6 @@ no_outlier <- function(df, col){
   Q1 <- stats::quantile(df$col, .25)
   Q3 <- stats::quantile(df$col, .75)
   IQR <- stats::IQR(df$col)
-  no_outliers_data <- subset(df, df$col > (Q1 - 1.5*IQR) & df$col < (Q3 + 1.5*IQR))
+  no_out <- subset(df, df$col > (Q1 - 1.5*IQR) & df$col < (Q3 + 1.5*IQR))
+  return(no_out)
 }
